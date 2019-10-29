f7d99999ff861301ab9cf47b8ec87b7fc0a96c68
CARLOS CESAR DA FERRAZ JUNIOR <carloscesardaferrazjunior@CARLOSs-MacBook-Pro.local>
2019-10-29 11:30:07 -0400

    first commit for Departments feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import Departments from './components/Departments'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src="" alt="logo" />
+        Market for Small Businesses
       </header>
+      <nav>Menu Options</nav>
+      <main>
+        <section id="departments">
+          <Departments />
+        </section>
+        <section id="departmentContents">
+          Department Contents
+        </section>
+        <section id="shoppingCart">
+          Shopping Cart
+        </section>
+      </main>
+      <footer>Copyright &copy;2019 Carlos Ferraz</footer>
     </div>
   );
 }
---
+++ src/components/Departments.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Department.css'
+
+class Departments extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>Departments</h1>
+            </div>
+        )
+    }
+}
+
+
+export default Departments;
