9e6421f73b4586fd21b217909b90588ce11a8314
CARLOS CESAR DA FERRAZ JUNIOR <carloscesardaferrazjunior@CARLOSs-MacBook-Pro.local>
2019-11-03 20:29:53 -0500

    Added logo with SVG

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ function App() {
   return (
     <div className="App">
       <header>
-        <img src="" alt="logo" />
+      <svg height="100" width="100">
+        <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
+      </svg>
         Market for Small Businesses
       </header>
       <nav><Menu departments={departments}/></nav>
